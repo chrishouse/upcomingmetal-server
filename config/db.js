e17c36b29ee77ec31e4ecc9972ad7dfa1803447a
Chris House <chrishouse83@protonmail.com>
2024-07-04 11:26:35 -0500

    change setInterval to setTimeout

--- config/db.js
+++ config/db.js
@@ -10,8 +10,8 @@ const connectDB = async () => {
 		console.log("MongoDB is Connected...");
 
 		// Run the get-metal script every 6 hours
-		setInterval(getMetal.getUpcoming, 1000 * 60 * 60 * 6);
-		setInterval(getMetal.getRecent, 1000 * 60 * 60 * 6);
+		setTimeout(getMetal.getUpcoming, 1000 * 60 * 60 * 6);
+		setTimeout(getMetal.getRecent, 1000 * 60 * 60 * 6);
 
 	} catch (err) {
 		console.error(err.message);
